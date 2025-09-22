8261bdf281a789b4d7274aa5a2e6ed49b4b88cda
Valeriia Ermakova <valeriia.sverige@gmail.com>
2025-09-22 19:31:37 +0200

    Added felhuntering Added function is_met_epic_loss_condition in Player class Introduced win count Created function to print win score between games

--- Classes_and_functions.py
+++ Classes_and_functions.py
@@ -4,10 +4,16 @@ import random
 class Player:
     def __init__(self):
         self.score = 0
+        self.win_count = 0
+
+    def roll(self):
+        pass
+
+    def is_met_epic_loss_condition(self):
+        return self.score >= 21
 
 
 class Human(Player):
-
     def roll(self):
         self.dice = random.randint(1, 6)
         self.score += self.dice
@@ -16,7 +22,6 @@ class Human(Player):
 
 
 class Dealer(Player):
-
     def roll(self, max_treshhold):
         while self.score <= max_treshhold:
             self.dice = random.randint(1, 6)
@@ -25,10 +30,22 @@ class Dealer(Player):
         return self.score
 
 
-def choose_winner(user_score, dealer_score):
+def choose_winner(user_score, dealer_score, user, dealer):
     if user_score > dealer_score:
         print("Du är vinnare!")
+        user.win_count += 1
     elif user_score < dealer_score:
-        print("Du förlorade. Dealern är vinnare!")
+        print("Du förlorade. Dealern är vinnare! ")
+        dealer.win_count += 1
     else:
-        print("Det är oavgjort!")
+        print("Det är oavgjort.")
+
+
+def print_win_count(user, dealer):
+    print(f"Du vann {user.win_count} spel och dealern vann {dealer.win_count} spel.")
+    if user.win_count > dealer.win_count:
+        print("Du leder! Bra jobbat! Låt oss fortsätta i nästa omgång!")
+    elif dealer.win_count > user.win_count:
+        print("Du kommer efter. Ge inte upp! Försök igen!")
+    else:
+        print("Det är oavgjort. Vem ska vinna nästa gång?")
--- main.py
+++ main.py
@@ -1,38 +1,49 @@
 import Classes_and_functions
+from Classes_and_functions import print_win_count
 
 print(
     """Välkommen till Tärningsspelet 21!\n
-    Du behöver slå tärningarna tills du är så nära till 21 poäng som möjligt utan att gå över.  Efter det vår Dealer rullar.
+    Du behöver slå tärningarna tills du är så nära till 21 poäng som möjligt utan att gå över. Efter det vår Dealer rullar.
     Man förlorar så snart som man når 21 poäng och annars vinner den som är närmast 21. 
     Du kan skriva 'rulla' för att rulla tärningen (värde 1-6) eller 'stanna' för att skicka turen till dealern eller 'avsluta' om du vill inte spella längre. Inga citattecken behövs.
     Lycka till!\n"""
 )
 
 user_choice = 0
+game_options = ["rulla", "stanna", "avsluta"]
 player1 = Classes_and_functions.Human()
 player2 = Classes_and_functions.Dealer()
 
 while user_choice != "avsluta":
-    user_choice = input("Skriva om du vill rulla, stanna eller avsluta:")
+    user_choice = input("Skriva om du vill rulla, stanna eller avsluta:").lower()
+    if user_choice not in game_options:
+        print(
+            "Error: Du skrev alternativ som inte stöds.\nFörsök igen och se till att du skriver något av följande untan citattecken eller andra symboler:\nrulla, stanna, avsluta.\nOroa dig inte, din progress är sparad."
+        )
+
     if user_choice == "rulla":
         final_user_score = player1.roll()
-
-        if final_user_score >= 21:
-            print(
-                f"Tyvärr, förlorade du det här spelet!\nDin score är {player1.score} som är mer eller lika 21.\nVi börjar over!"
-            )
+        if player1.is_met_epic_loss_condition():
+            print("Du har nått 21 poäng och förlorade. Dealern är vinnare!")
+            player2.win_count += 1
             player1.score = 0
             player2.score = 0
+            print_win_count(player1, player2)
             continue
 
     if user_choice == "stanna":
 
         final_dealer_score = player2.roll(17)
 
-        if final_dealer_score >= 21:
-            print(f"Du är vinnare!\n Dealern fick 21 eller mer! Vi börjar over!")
+        if player2.is_met_epic_loss_condition():
+            print(f"Du är vinnare! Dealern fick 21 eller mer poäng!")
+            player1.win_count += 1
             player1.score = 0
             player2.score = 0
+            print_win_count(player1, player2)
             continue
         else:
-            Classes_and_functions.choose_winner(final_user_score, final_dealer_score)
+            Classes_and_functions.choose_winner(
+                final_user_score, final_dealer_score, player1, player2
+            )
+            print_win_count(player1, player2)
